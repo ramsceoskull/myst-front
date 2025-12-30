54028ae87c4a71d033a39a16bd1db1d360612a59
Ramses Anceno <ramses.social0@gmail.com>
2025-12-29 22:06:12 -0600

    Main flow navigation implemented

--- app/src/main/java/com/example/myst/MainActivity.kt
+++ app/src/main/java/com/example/myst/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.myst.navigation.AppNavigation
 import com.example.myst.ui.theme.MystTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             MystTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AppNavigation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MystTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/myst/navigation/AppNavigation.kt
@@ -0,0 +1,25 @@
+package com.example.myst.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myst.MainScreen
+import com.example.myst.SplashScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = AppScreens.SplashScreen.route
+    ) {
+        composable(AppScreens.SplashScreen.route) {
+            SplashScreen(navController)
+        }
+        composable(AppScreens.MainScreen.route) {
+            MainScreen()
+        }
+    }
+}
