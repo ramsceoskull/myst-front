3a4bbd70d265f3d4859475924d575167c6a08e87
Ramses Anceno <ramses.social0@gmail.com>
2026-03-24 23:25:43 -0600

    Development of the main application flow

--- app/src/main/java/com/tenko/myst/MainActivity.kt
+++ app/src/main/java/com/tenko/myst/MainActivity.kt
@@ -1,47 +1,27 @@
 package com.tenko.myst
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.tenko.myst.ui.theme.TestTheme
+import com.tenko.myst.navigation.AppNavigation
+import com.tenko.myst.ui.theme.MystTheme
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TestTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MystTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { _ ->
+                    AppNavigation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestTheme {
-        Greeting("Android")
     }
 }
