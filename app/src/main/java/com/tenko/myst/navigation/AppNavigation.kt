789e29a9543b9c15fb281b5cd7e5027cc2e07500
Ramses Anceno <ramses.social0@gmail.com>
2026-04-13 22:56:51 -0600

    Token logic implemented for splash usage

--- app/src/main/java/com/tenko/myst/navigation/AppNavigation.kt
+++ app/src/main/java/com/tenko/myst/navigation/AppNavigation.kt
@@ -1,160 +1,153 @@
 package com.tenko.myst.navigation
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.tenko.myst.data.api.TokenManager
 import com.tenko.myst.data.view.AuthViewModel
 import com.tenko.myst.data.view.MedicineViewModel
 import com.tenko.myst.data.view.NotificationViewModel
-import com.tenko.myst.ui.components.BottomNavigationBar
 import com.tenko.myst.ui.components.NotificationsOverlay
 import com.tenko.myst.ui.screen.AddMedicationScreen
-import com.tenko.myst.ui.screen.AddMedicineTest
 import com.tenko.myst.ui.screen.AllNotificationsScreen
 import com.tenko.myst.ui.screen.CalendarScreen
-import com.tenko.myst.ui.screen.SplashScreen
-import com.tenko.myst.ui.screen.MainScreen
-import com.tenko.myst.ui.screen.LoginScreen
-import com.tenko.myst.ui.screen.ProfileScreen
-import com.tenko.myst.ui.screen.SignupScreen
 import com.tenko.myst.ui.screen.ChatScreen
 import com.tenko.myst.ui.screen.DoctorDetailsScreen
 import com.tenko.myst.ui.screen.DoctorsScreen
 import com.tenko.myst.ui.screen.EmailSentScreen
+import com.tenko.myst.ui.screen.LoginScreen
+import com.tenko.myst.ui.screen.MainScreen
 import com.tenko.myst.ui.screen.NotificationDetailScreen
 import com.tenko.myst.ui.screen.PdfViewerScreen
+import com.tenko.myst.ui.screen.ProfileScreen
+import com.tenko.myst.ui.screen.SignupScreen
+import com.tenko.myst.ui.screen.SplashScreen
+import com.tenko.myst.ui.screen.UpdateProfileScreen
 import com.tenko.myst.ui.screen.doctorsList
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+
 @Composable
-fun AppNavigation() {
+fun AppNavigation(tokenManager: TokenManager) {
     val navController = rememberNavController()
     val notificationViewModel = viewModel<NotificationViewModel>()
     val authViewModel = viewModel<AuthViewModel>()
     val medicineViewModel = viewModel<MedicineViewModel>()
-    /*val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
 
-    val screensWithBottomBar = listOf(
-        AppScreens.MainScreen.route,
-        AppScreens.ProfileScreen.route,
-        AppScreens.ChatScreen.route,
-        AppScreens.DoctorsScreen.route,
-        AppScreens.CalendarScreen.route
-    )
+    // Observamos el token del DataStore
+    val token by tokenManager.getToken.collectAsState(initial = "loading")
 
-    Scaffold(
-        bottomBar = {
-            if (currentRoute in screensWithBottomBar)
-                BottomNavigationBar(navController)
+    when(token) {
+        "loading" -> {
+            SplashScreen()
         }
-    ) {  _ ->*/
-    NavHost(
-        navController = navController,
-        startDestination = AppScreens.SplashScreen.route
-    ) {
-        composable(AppScreens.SplashScreen.route) { SplashScreen(navController) }
-        composable(AppScreens.SignupScreen.route) { SignupScreen(navController, authViewModel) }
-        composable(AppScreens.LoginScreen.route) { LoginScreen(navController, authViewModel) }
-        composable(AppScreens.ProfileScreen.route) { ProfileScreen(navController) }
-        composable(AppScreens.ChatScreen.route) { ChatScreen(navController) }
-        composable(AppScreens.CalendarScreen.route) { CalendarScreen(navController) }
-        composable(AppScreens.DoctorsScreen.route) { DoctorsScreen(navController) }
-        composable(AppScreens.AddMedicationScreen.route) {
-            AddMedicationScreen(
-                viewModel = medicineViewModel,
-                onClose = { navController.popBackStack() },
-            )
-        }
-//        composable("confirm") { ConfirmScreen(navController, formStateViewModel) }
-        composable(AppScreens.MainScreen.route) { MainScreen(navController, notificationViewModel, medicineViewModel) }
-        composable("terms") {
-            PdfViewerScreen(
-                pdfUrl = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
-                onAccept = {
-                    navController.previousBackStackEntry
-                        ?.savedStateHandle
-                        ?.set("termsAccepted", true)
+        else -> {
+            val startTrigger = if(token == null) AppScreens.LoginScreen.route else AppScreens.MainScreen.route
 
-                    navController.popBackStack()
-                },
-                onDismiss = {
-                    navController.popBackStack()
+            NavHost(
+                navController = navController,
+                startDestination = startTrigger
+            ) {
+                composable(AppScreens.SplashScreen.route) { SplashScreen() }
+                composable(AppScreens.SignupScreen.route) { SignupScreen(navController, authViewModel) }
+                composable(AppScreens.LoginScreen.route) { LoginScreen(navController, authViewModel) }
+                composable(AppScreens.ProfileScreen.route) { ProfileScreen(navController,authViewModel) }
+                composable(AppScreens.UpdateProfileScreen.route) { UpdateProfileScreen(navController, authViewModel, tokenManager) }
+                composable(AppScreens.ChatScreen.route) { ChatScreen(navController) }
+                composable(AppScreens.CalendarScreen.route) { CalendarScreen(navController) }
+                composable(AppScreens.DoctorsScreen.route) { DoctorsScreen(navController) }
+                composable(AppScreens.AddMedicationScreen.route) {
+                    AddMedicationScreen(
+                        viewModel = medicineViewModel,
+                        onClose = { navController.popBackStack() },
+                    )
                 }
-            )
-        }
-        composable(AppScreens.NotificationsOverlay.route) {
-            NotificationsOverlay(
-                viewModel = notificationViewModel,
-                onDismiss = { mutableStateOf(false) },
-                onSeeAllClick = { navController.navigate(AppScreens.AllNotificationsScreen.route) },
-                onNotificationClick = {
-                    navController.navigate("${AppScreens.NotificationDetailsScreen.route}/${it.id}")
-                }
-            )
-        }
-        composable(AppScreens.AllNotificationsScreen.route) {
-            AllNotificationsScreen(
-                viewModel = notificationViewModel,
-                onBack = { navController.popBackStack() },
-                onNotificationClick = {
-                    navController.navigate("${AppScreens.NotificationDetailsScreen.route}/${it.id}")
-                }
-            )
-        }
-        composable(
-            route = AppScreens.NotificationDetailsScreen.route,
-            arguments = listOf(
-                navArgument("notificationId") {
-                    type = NavType.IntType
-                }
-            )
-        ) { backStackEntry ->
-            val notificationId = backStackEntry.arguments?.getInt("notificationId")
-            val notification = notificationViewModel.notifications.first { it.id == notificationId }
-            NotificationDetailScreen(notification)
-        }
-        composable(AppScreens.ForgotPasswordScreen.route) {
-            EmailSentScreen(
-                title = "Se te ha enviado un correo",
-                description = "Favor de revisar tu bandeja de entrada o tu carpeta de spam para restablecer tu contraseña",
-                actionLabel = "Iniciar Sesión",
-                onClick = { navController.navigate(AppScreens.LoginScreen.route) },
-                onResendClick = { /* Lógica para reenviar el correo */ }
-            )
-        }
-        composable(AppScreens.ValidateEmailScreen.route) {
-            EmailSentScreen(
-                title = "Confirma tu correo electrónico",
-                description = "Hemos enviado un correo de confirmación a tu dirección. Por favor, revisa tu bandeja de entrada o tu carpeta de spam para activar tu cuenta.",
-                actionLabel = "Iniciar Sesión",
-                onClick = { navController.navigate(AppScreens.LoginScreen.route) },
-                onResendClick = { /* Lógica para reenviar el correo */ }
-            )
-        }
-        composable(
-            route = AppScreens.DoctorDetailsScreen.route,
-            arguments = listOf(
-                navArgument("doctorId") {
-                    type = NavType.IntType
-                }
-            )
-        ) { backStackEntry ->
-            val doctorId = backStackEntry.arguments?.getInt("doctorId")
-            val doctor = doctorsList.find { it.id == doctorId }
+                composable(AppScreens.MainScreen.route) { MainScreen(navController, notificationViewModel, medicineViewModel) }
+                composable("terms") {
+                    PdfViewerScreen(
+                        pdfUrl = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+                        onAccept = {
+                            navController.previousBackStackEntry
+                                ?.savedStateHandle
+                                ?.set("termsAccepted", true)
 
-            doctor?.let {
-                DoctorDetailsScreen(navController, it)
+                            navController.popBackStack()
+                        },
+                        onDismiss = {
+                            navController.popBackStack()
+                        }
+                    )
+                }
+                composable(AppScreens.NotificationsOverlay.route) {
+                    NotificationsOverlay(
+                        viewModel = notificationViewModel,
+                        onDismiss = { mutableStateOf(false) },
+                        onSeeAllClick = { navController.navigate(AppScreens.AllNotificationsScreen.route) },
+                        onNotificationClick = {
+                            navController.navigate("${AppScreens.NotificationDetailsScreen.route}/${it.id}")
+                        }
+                    )
+                }
+                composable(AppScreens.AllNotificationsScreen.route) {
+                    AllNotificationsScreen(
+                        viewModel = notificationViewModel,
+                        onBack = { navController.popBackStack() },
+                        onNotificationClick = {
+                            navController.navigate("${AppScreens.NotificationDetailsScreen.route}/${it.id}")
+                        }
+                    )
+                }
+                composable(
+                    route = AppScreens.NotificationDetailsScreen.route,
+                    arguments = listOf(
+                        navArgument("notificationId") {
+                            type = NavType.IntType
+                        }
+                    )
+                ) { backStackEntry ->
+                    val notificationId = backStackEntry.arguments?.getInt("notificationId")
+                    val notification = notificationViewModel.notifications.first { it.id == notificationId }
+                    NotificationDetailScreen(notification)
+                }
+                composable(AppScreens.ForgotPasswordScreen.route) {
+                    EmailSentScreen(
+                        title = "Se te ha enviado un correo",
+                        description = "Favor de revisar tu bandeja de entrada o tu carpeta de spam para restablecer tu contraseña",
+                        actionLabel = "Iniciar Sesión",
+                        onClick = { navController.navigate(AppScreens.LoginScreen.route) },
+                        onResendClick = { /* Lógica para reenviar el correo */ }
+                    )
+                }
+                composable(AppScreens.ValidateEmailScreen.route) {
+                    EmailSentScreen(
+                        title = "Confirma tu correo electrónico",
+                        description = "Hemos enviado un correo de confirmación a tu dirección. Por favor, revisa tu bandeja de entrada o tu carpeta de spam para activar tu cuenta.",
+                        actionLabel = "Iniciar Sesión",
+                        onClick = { navController.navigate(AppScreens.LoginScreen.route) },
+                        onResendClick = { /* Lógica para reenviar el correo */ }
+                    )
+                }
+                composable(
+                    route = AppScreens.DoctorDetailsScreen.route,
+                    arguments = listOf(
+                        navArgument("doctorId") {
+                            type = NavType.IntType
+                        }
+                    )
+                ) { backStackEntry ->
+                    val doctorId = backStackEntry.arguments?.getInt("doctorId")
+                    val doctor = doctorsList.find { it.id == doctorId }
+
+                    doctor?.let {
+                        DoctorDetailsScreen(navController, it)
+                    }
+                }
             }
         }
     }
-//    }
 }
