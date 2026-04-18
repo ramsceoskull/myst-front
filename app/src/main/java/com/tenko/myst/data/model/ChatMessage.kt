95ac36b5db219e55315bbcb1c421cbb82c3beb05
Ramses Anceno <ramses.social0@gmail.com>
2026-04-17 19:00:10 -0600

    Question reference field added

--- app/src/main/java/com/tenko/myst/data/model/ChatMessage.kt
+++ app/src/main/java/com/tenko/myst/data/model/ChatMessage.kt
@@ -3,5 +3,6 @@ package com.tenko.myst.data.model
 data class ChatMessage(
     val id: Long,
     val text: String,
-    val isUser: Boolean
+    val isUser: Boolean,
+    val questionRef: ClinicalQuestion? = null
 )
