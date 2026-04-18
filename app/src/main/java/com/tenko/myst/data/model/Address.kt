1429789b3b4e5d27650d074c8c814034bb900403
Ramses Anceno <ramses.social0@gmail.com>
2026-04-18 10:23:29 -0600

    Doctor's fields

---
+++ app/src/main/java/com/tenko/myst/data/model/Address.kt
@@ -0,0 +1,10 @@
+package com.tenko.myst.data.model
+
+data class Address(
+    val street: String = "",
+    val city: String = "",
+    val state: String = "",
+    val zipCode: String = "",
+    val country: String = "",
+    val phoneNumer: String = ""
+)
--- app/src/main/java/com/tenko/myst/data/model/Doctor.kt
+++ app/src/main/java/com/tenko/myst/data/model/Doctor.kt
@@ -4,10 +4,12 @@ import com.tenko.myst.R
 
 data class Doctor(
     val id: Int,
-    val imageRes: Int = R.drawable.doctor0,
-    val name: String = "Dr. John Doe",
-    val subtitle: String = "Cardiologist",
-    val about: String = "Dr. John Doe is a highly experienced cardiologist with over 20 years of practice. He specializes in treating heart conditions and has a passion for helping patients improve their cardiovascular health.",
+    val avatar: Int = R.drawable.doctor0,
+    val name: String = "John",
+    val lastName: String = "Doe",
     val email: String = "ramsesrame21@gmail.com",
-    val phoneNumber: Number = 3333943613
+    val about: String = "Dr. John Doe is a highly experienced cardiologist with over 20 years of practice. He specializes in treating heart conditions and has a passion for helping patients improve their cardiovascular health.",
+    val speciality: Speciality? = null,
+    val genre: Genre? = null,
+    val address: Address? = null,
 )
